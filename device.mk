ac8f8407b57be0b945de88db34ce7f13c7ebbdfd
Kalyan Sundhar <kayes.kalyan@gmail.com>
2018-03-07 13:21:28 +0530

    Removed dependancy on phone-xxhdpi-3072-hwui-memory.mk

--- device.mk
+++ device.mk
@@ -17,7 +17,18 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Dalvik/HWUI configs
-$(call inherit-product, frameworks/native/build/phone-xxhdpi-3072-hwui-memory.mk)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+	ro.hwui.text_large_cache_height=1024
 
 # Call the proprietary setup
 $(call inherit-product, vendor/motorola/montana/montana-vendor.mk)
@@ -124,6 +135,9 @@ PRODUCT_COPY_FILES += \
 # CM
 PRODUCT_PACKAGES += \
     MotoActions
+
+PRODUCT_PACKAGES += \
+    Launcher3
 
 # Display
 PRODUCT_PACKAGES += \
