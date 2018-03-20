aeaa863a5445c6b2f1f7ed5321ee6f18fd0dec07
Kalyan Sundhar <kayes.kalyan@gmail.com>
2018-03-20 10:55:41 +0530

    Commented out kernel headers include.

--- device.mk
+++ device.mk
@@ -427,4 +427,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
-PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8996/kernel-headers
+#PRODUCT_VENDOR_KERNEL_HEADERS := hardware/qcom/msm8996/kernel-headers
