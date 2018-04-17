095d12eb3e41f4452437637a1b50864f3ca58b07
Kalyan Sundhar <kayes.kalyan@gmail.com>
2018-04-17 13:44:43 +0530

    Modified to call /system/vendor/bin/check_features.sh

--- releasetools.py
+++ releasetools.py
@@ -1,3 +1,4 @@
+# Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,27 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import re
+
+def IncrementalOTA_InstallEnd(info):
+  ReplaceDeviceConfig(info)
 
 def FullOTA_InstallEnd(info):
-  info.script.AppendExtra('if (getprop("ro.boot.radio") != "EMEA") then')
-  info.script.Print("Non-EMEA variant detected !!!")
-  info.script.Print("Disabling NFC configuration...")
-  info.script.Print("Credits to Kalyansundhar, GoldeneyeS2")
+  ReplaceDeviceConfig(info)
+
+def ReplaceDeviceConfig(info):
   info.script.Mount("/system")
-  info.script.AppendExtra('delete_recursive("/system/app/NfcSetup");')
-  info.script.AppendExtra('delete_recursive("/system/app/NfcNci");')
-  info.script.AppendExtra('delete("/system/lib/libnfc-nci.so");')
-  info.script.AppendExtra('delete("/system/lib/libnfc_nci_jni.so");')
-  info.script.AppendExtra('delete("/system/etc/libnfc-nxp.conf");')
-  info.script.AppendExtra('delete("/system/etc/libnfc-nxp_ds.conf");')
-  info.script.AppendExtra('delete("/system/etc/libnfc-brcm.conf");')
-  info.script.AppendExtra('delete("/system/framework/com.android.nfc_extras.jar");')
-  info.script.AppendExtra('delete("/system/framework/com.gsma.services.nfc.jar");')
-  info.script.AppendExtra('delete("/system/etc/permissions/android.hardware.nfc.xml");')
-  info.script.AppendExtra('delete("/system/etc/permissions/android.hardware.nfc.hce.xml");')
-  info.script.AppendExtra('delete("/system/etc/permissions/com.android.nfc_extras.xml");')
-  info.script.AppendExtra('delete("/system/etc/permissions/com.nxp.mifare.xml");')
+  info.script.AppendExtra('ui_print("Checking NFC support");')
+  info.script.AppendExtra('run_program("/sbin/sh", "/system/vendor/bin/check_features.sh");')
   info.script.Unmount("/system")
-  info.script.Print("Disabled.")
-  info.script.AppendExtra('endif;')
+  
