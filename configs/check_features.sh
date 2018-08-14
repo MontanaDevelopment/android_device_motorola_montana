e48c23a9081f4762a85cfb772725c81a7e86a945
Pedro Kalil <kalilgamer0@gmail.com>
2018-08-14 00:12:57 +0000

    msm8953-common: refactor check_features for cedric

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -1,20 +1,23 @@
 #!/sbin/sh
 
 sku=`getprop ro.boot.hardware.sku`
+device=`getprop ro.boot.device`
 
-if [ "$sku" = "XT1687" ]; then
-    # XT1687 doesn't have NFC chip
+if [ "$sku" == "XT1687" ] || [ "$device" = "cedric" ]; then
+    # XT1687 and cedric don't have NFC chips
     rm /system/vendor/etc/permissions/android.hardware.nfc.xml
     rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
-else
+fi
+
+if [ "$sku" != "XT1687" ]; then
     # Only XT1687 variant got a compass
     rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
 fi
 
-if ! [ "$sku" = "XT1683" ]; then
-    # Others variants doesn't have DTV support
+if [ "$sku" != "XT1683" ]; then
+    # Only XT1683 has DTV support
     rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
     rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
     rm /system/vendor/lib/libdtvtuner.so
