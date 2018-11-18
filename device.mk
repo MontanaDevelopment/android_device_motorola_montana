734426aef3d39fff3177d3166c0c12b2c5579c6b
kubersharma001 <kubersharma001@gmail.com>
2018-11-18 16:51:35 +0530

    montana: import nfc configs from stock O and adapt for pie
    from potter
    https://github.com/boulzordev/android_device_motorola_potter/commit/b99869c0c6f765d8aed0024aacb9dac0d41d827b
    
    Change-Id: I140f46815912c155bb177f2fcb788f59e1b4a707

--- device.mk
+++ device.mk
@@ -269,7 +269,7 @@ PRODUCT_COPY_FILES += \
 
 # NFC Configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-brcm.conf:system/vendor/etc/libnfc-brcm.conf \
+    $(LOCAL_PATH)/configs/libnfc-nci.conf:system/vendor/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/vendor/etc/libnfc-nxp.conf
 
 # Netutils
