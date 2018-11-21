a831be72a8db13633af60c080a21589a7729abea
kubersharma001 <kubersharma001@gmail.com>
2018-11-21 17:27:26 +0530

    montana: update NFC packages
    Change-Id: I27a629830d18e6ee452956ac16a0887b560bc18a

--- device.mk
+++ device.mk
@@ -264,13 +264,21 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_8956.xml:system/vendor/etc/media_profiles_8956.xml
 
 # NFC
-PRODUCT_COPY_FILES += \
-    frameworks/base/nfc-extras/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml
+PRODUCT_PACKAGES += \
+    libnfc \
+    com.android.nfc_extras \
+    NfcNci \
+    nqnfcee_access.xml \
+    nqnfcse_access.xml \
+    SecureElement \
+    Tag \
+    com.android.nfc_extras \
+    android.hardware.nfc@1.1-service
 
 # NFC Configuration
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/libnfc-nci.conf:system/vendor/etc/libnfc-nci.conf \
-    $(LOCAL_PATH)/configs/libnfc-nxp.conf:system/vendor/etc/libnfc-nxp.conf
+    $(LOCAL_PATH)/configs/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 # Netutils
 PRODUCT_PACKAGES += \
@@ -278,15 +286,6 @@ PRODUCT_PACKAGES += \
     netutils-wrapper-1.0 \
     libandroid_net \
     libandroid_net_32
-
-PRODUCT_PACKAGES += \
-    libnfc \
-    libnfc_jni \
-    nfc_nci.msm8937 \
-    NfcNci \
-    Tag \
-    com.android.nfc_extras \
-    android.hardware.nfc@1.0-impl
 
 # OMX
 PRODUCT_PACKAGES += \
