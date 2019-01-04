7dc274540a238e388f3d0482e71cab8d75131477
AsD Monio <asdmonio@gmail.com>
2019-01-04 18:08:25 +0100

    montana: Build the camera wrapper
    Change-Id: I0ffd4123306009353fcfb49a2639a3637a5f9581

--- device.mk
+++ device.mk
@@ -107,6 +107,7 @@ PRODUCT_PACKAGES += \
     Snap \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
+    camera.msm8937 \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
