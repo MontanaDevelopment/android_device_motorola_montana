464e38438a23ac4ed762add0238afc30b7a40d59
AsD Monio <asdmonio@gmail.com>
2019-01-04 17:05:11 +0100

    Revert "montana: Update camera shim"
    This reverts commit 2665ecca35ef5f97b3570dbe85abe60a4ee35cfe.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -227,8 +227,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 # Shim
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/adspd|libshim_adsp.so \
-    /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so \
-    /system/vendor/lib/libHui.so|libshim_camera_hal.so
+    /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so
 
 # Thermal
 USE_DEVICE_SPECIFIC_THERMAL := true
