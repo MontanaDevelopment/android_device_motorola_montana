d33e64baac00247b2d5fb1c4cc00bbf1086f5125
AsD Monio <asdmonio@gmail.com>
2019-01-05 20:45:30 +0100

    Revert "montana: Build the camera wrapper"
    This reverts commit 7dc274540a238e388f3d0482e71cab8d75131477.
    
    Change-Id: I2889a996820739b1bc3535c4608431f66f2ebfdb

--- device.mk
+++ device.mk
@@ -106,7 +106,6 @@ PRODUCT_PACKAGES += \
     Snap \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
-    camera.msm8937 \
     libshim_camera \
     android.hardware.camera.provider@2.4-impl \
     vendor.qti.hardware.camera.device@1.0 \
