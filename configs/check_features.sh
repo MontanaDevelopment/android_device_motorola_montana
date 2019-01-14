7f7519c26d38c1c9b97badb8164a560aac2601c6
AsD Monio <asdmonio@gmail.com>
2019-01-14 20:40:54 +0100

    montana: Change NFC removing script logic
    * Now we only check the radio if the device is montana
    * The other checks are left as they were in the common trees before
      bfa38ef537dc739e7ecfedc3756a59c41f6b7fff
    
    Change-Id: Iaef140b42cf08a32f0e598482f4640492e2a4bef

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -4,13 +4,23 @@ sku=`getprop ro.boot.hardware.sku`
 device=`getprop ro.boot.device`
 radio=`getprop ro.boot.radio`
 
-if [ "$sku" == "XT1687" ] || [ "$device" = "cedric" ] || [ "$radio" != "EMEA" ] && [ "$radio" != "APAC" ]; then
-    # XT1687 and cedric don't have NFC chips
-    # On montana only EMEA and APAC radios have NFC
+function remove_nfc () {
     rm /system/vendor/etc/permissions/android.hardware.nfc.xml
     rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
+}
+
+if [ "$device" == "montana" ]; then
+    if [ "$radio" != "EMEA" ] && [ "$radio" != "APAC" ]; then
+        # On montana only EMEA and APAC radios have NFC
+        remove_nfc
+    fi
+fi
+
+if [ "$sku" == "XT1687" ] || [ "$device" = "cedric" ]; then
+    # XT1687 and cedric don't have NFC chips
+    remove_nfc
 fi
 
 if [ "$sku" != "XT1687" ]; then
