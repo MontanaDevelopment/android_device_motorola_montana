2ce4fc0061afab7b43e1293325930ff9e681455e
ZJRDroid <priv.zjr@gmail.com>
2019-01-17 21:37:19 +0000

    Use Gcam by default

--- device.mk
+++ device.mk
@@ -99,7 +99,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     libbson \
-    Snap \
+    GCamera \
     camera.device@1.0-impl \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl \
