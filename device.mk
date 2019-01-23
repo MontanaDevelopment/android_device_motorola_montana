2d9dbd0d15362d22683dbec906b6f6878b086237
Vachounet <vachounet@live.fr>
2019-01-23 16:19:05 +0100

    montana: build audio service
    Change-Id: I3db36f74451a64b3c1e8eed4cce519891a71a8a2

--- device.mk
+++ device.mk
@@ -87,6 +87,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl
 
