386275520ea9c95dfc067ff8d7b5f557e315ce1b
Vachounet <vachounet@live.fr>
2019-01-23 16:22:54 +0100

    montana: kang gemini health hal
    Change-Id: I619b9e96dad69f24f373dd203b4ee1cf2248a824

--- device.mk
+++ device.mk
@@ -210,9 +210,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@1.0-convert \
-    android.hardware.health@1.0-impl \
-    android.hardware.health@1.0-service
+    android.hardware.health@2.0-service.montana
 
 # HIDL
 PRODUCT_PACKAGES += \
