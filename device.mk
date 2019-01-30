d8846129015e3339ccf591957a0e5bb8172b568a
AsD Monio <asdmonio@gmail.com>
2019-01-30 14:41:22 +0100

    montana: Don't build Bluetooth HALs
    Change-Id: Ifb813c28c8165d65996f468a170e22db9502988d

--- device.mk
+++ device.mk
@@ -104,8 +104,6 @@ PRODUCT_COPY_FILES +=  \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor \
-    android.hardware.bluetooth@1.0-impl \
-    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
