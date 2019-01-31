71a726ae072f0c15f72acd84268c94842c6bfb63
AsD Monio <asdmonio@gmail.com>
2019-01-31 15:47:40 +0100

    montana: Add libjustshoot path to shims
    For some reason libjustshoot.so is present in both /system/lib and /system/vendor/lib, both need to be shimmed
    
    Change-Id: Ie6050deedc411b6f6c7a02b7d7068d0044255eff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -228,6 +228,7 @@ TARGET_LD_SHIM_LIBS := \
     /system/vendor/bin/adspd|libshim_adsp.so \
     /system/vendor/lib/libmmcamera_ppeiscore.so|libshim_camera.so \
     /system/vendor/lib/libmot_gpu_mapper.so|libgpu_mapper_shim.so \
+    /system/lib/libjustshoot.so|libjustshoot_shim.so \
     /system/vendor/lib/libjustshoot.so|libjustshoot_shim.so \
     /system/vendor/lib64/libmdmcutback.so|libqsap_shim.so
 
