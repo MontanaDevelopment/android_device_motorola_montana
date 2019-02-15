aeec66e186c2ebd435ec733ee0978a3a14ed41d5
RaySlash <stevemathewjoy@gmail.com>
2019-02-15 12:41:40 +0530

    montana: Add BUILD_FINGERPRINT

--- lineage_montana.mk
+++ lineage_montana.mk
@@ -42,7 +42,12 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
 PRODUCT_SYSTEM_PROPERTY_BLACKLIST := ro.product.model
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME="Moto G5s"
+    PRODUCT_NAME="montana"
+    TARGET_NAME="montana"
+    PRIVATE_BUILD_DESC="montana-userdebug 8.1.0 OPM7.181205.001 0fc62393eb release-keys"
+
+# Get BUILD_FINGERPRINT value
+BUILD_FINGERPRINT := "motorola/montana/montana:8.1.0/OPM7.181205.001/0fc62393eb:userdebug/release-keys"
 
 # for specific
 $(call inherit-product, vendor/motorola/montana/montana-vendor.mk)
