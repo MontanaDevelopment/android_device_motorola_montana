462e562a243922246d1d889d7f822e22176baf62
KalilDev <kalilgamer0@gmail.com>
2019-02-16 13:09:57 -0200

    montana: Ship v27 libui (1/2)
    * Needed by lib-imsvideocodec.so
    
    Change-Id: I291da988a6c92ed32f5d38366393359401022826

--- device.mk
+++ device.mk
@@ -363,6 +363,9 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full \
     libxml2
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libui-v27.so
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy \
