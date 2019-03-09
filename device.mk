1e935b9423b9c36ec0d15c86b03b878eb632a47a
AsD Monio <asdmonio@gmail.com>
2019-03-09 22:15:14 +0100

    montana: Build WPA Supplicant HAL
    Change-Id: Id0919277fa798d6377b0d4d1d9d7387245da809a

--- device.mk
+++ device.mk
@@ -389,6 +389,7 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
+    android.hardware.wifi.supplicant@1.0 \
     hostapd \
     libqsap_sdk \
     libwpa_client \
