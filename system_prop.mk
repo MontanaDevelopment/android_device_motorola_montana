4b958275b6b1ab25aaaca693335a0ece12d5a85f
Vachounet <vachounet@live.fr>
2019-03-09 22:00:59 +0000

    montana: add back FM support

--- system_prop.mk
+++ system_prop.mk
@@ -41,6 +41,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.hfp.client=1 \
     qcom.bluetooth.soc=smd \
+    vendor.qcom.bluetooth.soc=smd \
     qcom.bt.le_dev_pwr_class=1 \
     ro.bluetooth.hfp.ver=1.7 \
     ro.qualcomm.bt.hci_transport=smd \
