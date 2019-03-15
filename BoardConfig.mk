456db2b386176e3b064ab8c3bd9a2d94cc866cae
JarlPenguin <jarlpenguin@outlook.com>
2019-03-15 19:53:19 +0300

    Add TWRP theme

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -245,3 +245,6 @@ WIFI_DRIVER_FW_PATH_AP           := "ap"
 WIFI_DRIVER_FW_PATH_STA          := "sta"
 WIFI_DRIVER_FW_PATH_P2P          := "p2p"
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
+
+# TWRP
+TW_THEME := portrait_hdpi
