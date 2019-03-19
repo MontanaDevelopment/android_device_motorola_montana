73345f3bc158c61b848c893548d1240c985c87a2
JarlPenguin <jarlpenguin@outlook.com>
2019-03-19 12:25:35 +0300

    Switch libraries

--- cryptfs_hw/Android.mk
+++ cryptfs_hw/Android.mk
@@ -1,6 +1,24 @@
-ifeq ($(TARGET_HW_DISK_ENCRYPTION),true)
+#
+# Copyright (c) 2014, The Linux Foundation. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
+
+LOCAL_PROPRIETARY_MODULE := true
 
 sourceFiles := \
                cryptfs_hw.c
@@ -9,9 +27,7 @@ commonSharedLibraries := \
                         libcutils \
                         libutils \
                         libdl \
-                        libhardware \
-                        liblog
-
+                        libhardware
 commonIncludes := \
                   hardware/libhardware/include/hardware/
 
@@ -35,4 +51,3 @@ LOCAL_CFLAGS += -DUSE_ICE_FOR_STORAGE_ENCRYPTION
 endif
 
 include $(BUILD_SHARED_LIBRARY)
-endif
