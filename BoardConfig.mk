02d94613d4ca6336707c06ba532ba1f8f4cfcc8f
joelh <joelh@users.noreply.github.com>
2019-04-28 21:20:33 +0300

    montana: stop using build.prop to set vendor security patch
    * Use BoardConfig.mk instead

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -219,6 +219,9 @@ TARGET_RIL_VARIANT := caf
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)/releasetools
 
+# Security patch level
+VENDOR_SECURITY_PATCH := 2019-02-01
+
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
