295e52f6aa865aa9cbb10888f92462a3f9c9e6cc
JarlPenguin <jarlpenguin@outlook.com>
2019-05-15 14:54:17 +0300

    montana: correct info in check_features.sh
    * Remove DTV always for now
    
    Signed-off-by: JarlPenguin <jarlpenguin@outlook.com>

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -1,34 +1,21 @@
 #!/sbin/sh
 
 sku=`getprop ro.boot.hardware.sku`
-device=`getprop ro.boot.device`
-radio=`getprop ro.boot.radio`
 
-remove_nfc() {
+if [ "$sku" = "XT1792" ]; then
+    # XT1792 doesn't have NFC
     rm /system/vendor/etc/permissions/android.hardware.nfc.xml
     rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
 }
 
-if [ "$sku" == "XT1687" ] || [ "$device" == "cedric" ]; then
-    # XT1687 and cedric don't have NFC chips
+if [ "$sku" == "XT1792" ]
+    # XT1792 doesn't have NFC
     remove_nfc
 fi
 
-if [ "$device" == "montana" ]; then
-    if [ "$radio" != "EMEA" ] && [ "$radio" != "APAC" ]; then
-        # On montana only EMEA and APAC radio models have NFC
-        remove_nfc
-    fi
-fi
-
-if [ "$sku" != "XT1687" ]; then
-    # Only XT1687 variant got a compass
-    rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
-fi
-
-if [ "$sku" != "XT1683" ]; then
+#if [ "$sku" != "XT1683" ]; then
     # Only XT1683 has DTV support
     rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
     rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
@@ -38,5 +25,9 @@ if [ "$sku" != "XT1683" ]; then
     rm /system/vendor/lib64/libdtvhal.so
     rm -r /system/vendor/app/DTVPlayer
     rm -r /system/vendor/app/DTVService
-fi
+#fi
 
+if [ "$sku" != "XT1792" ]; then
+    # Only XT1792 variant has a compass
+    rm /system/vendor/etc/permissions/android.hardware.sensor.compass.xml
+fi
