a267bdc7f40be648b7918658ae5d9d36edf1540d
OlivierB <olivierlohan@gmail.com>
2019-05-16 15:43:16 +0200

    Update lineage_montana.mk

--- lineage_montana.mk
+++ lineage_montana.mk
@@ -23,7 +23,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/montana/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -31,7 +31,7 @@ TARGET_SCREEN_HEIGHT := 1920
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := montana
-PRODUCT_NAME := lineage_montana
+PRODUCT_NAME := rr_montana
 PRODUCT_BRAND := motorola
 PRODUCT_MANUFACTURER := motorola
 PRODUCT_MODEL := Moto G5S
