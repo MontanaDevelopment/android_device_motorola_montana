2369712100936bfa11675012f03cb905a9e4d6f8
JarlPenguin <jarlpenguin@outlook.com>
2019-05-21 11:30:27 +0300

    montana: add XT1795 to SKUs without NFC

--- configs/check_features.sh
+++ configs/check_features.sh
@@ -8,24 +8,35 @@ if [ "$sku" = "XT1792" ]; then
     rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
     rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
     rm -r /system/app/NfcNci
-}
+fi
 
-if [ "$sku" == "XT1792" ]
+if [ "$sku" == "XT1792" ]; then
     # XT1792 doesn't have NFC
     remove_nfc
 fi
 
-#if [ "$sku" != "XT1683" ]; then
-    # Only XT1683 has DTV support
-    rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
-    rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
-    rm /system/vendor/lib/libdtvtuner.so
-    rm /system/vendor/lib64/libdtvtuner.so
-    rm /system/vendor/lib/libdtvhal.so
-    rm /system/vendor/lib64/libdtvhal.so
-    rm -r /system/vendor/app/DTVPlayer
-    rm -r /system/vendor/app/DTVService
-#fi
+if [ "$sku" == "XT1795" ]; then
+    # XT1795 doesn't have NFC
+    rm /system/vendor/etc/permissions/android.hardware.nfc.xml
+    rm /system/vendor/etc/permissions/android.hardware.nfc.hce.xml
+    rm /system/vendor/etc/permissions/com.android.nfc_extras.xml
+    rm -r /system/app/NfcNci
+fi
+
+if [ "$sku" == "XT1795" ]; then
+    # XT1795 doesn't have NFC
+    remove_nfc
+fi
+
+# Remove DTB stuff
+rm /system/vendor/etc/permissions/com.motorola.hardware.dtv.xml
+rm /system/vendor/etc/permissions/mot_dtv_permissions.xml
+rm /system/vendor/lib/libdtvtuner.so
+rm /system/vendor/lib64/libdtvtuner.so
+rm /system/vendor/lib/libdtvhal.so
+rm /system/vendor/lib64/libdtvhal.so
+rm -r /system/vendor/app/DTVPlayer
+rm -r /system/vendor/app/DTVService
 
 if [ "$sku" != "XT1792" ]; then
     # Only XT1792 variant has a compass
