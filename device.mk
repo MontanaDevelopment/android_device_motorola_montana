3b5cf2151a25754ffbf4f9dcdccadbe16792ce07
AsD Monio <asdmonio@gmail.com>
2019-05-26 17:07:08 +0200

    montana: Import SDK 27 libbinder
    Change-Id: I47307cc503ce6f00fd673dd5145a370b2d69a0cb

--- device.mk
+++ device.mk
@@ -366,7 +366,8 @@ PRODUCT_PACKAGES += \
     libxml2
 
 PRODUCT_COPY_FILES += \
-    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libui-v27.so
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/libui.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libui-v27.so \
+    prebuilts/vndk/v27/arm64/arch-arm64-armv8-a/shared/vndk-core/libbinder.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libbinder-v27.so
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
