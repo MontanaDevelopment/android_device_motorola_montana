f07cd6903c5567a2330da6c63939b9099897ed1b
Jarl-Penguin <jarlpenguin@outlook.com>
2019-06-21 00:14:52 +0300

    montana: compile kernel with clang
    Signed-off-by: Jarl-Penguin <jarlpenguin@outlook.com>
    Change-Id: I21b2f641e193fd8a1f6bdebe3f10d719a7f5ee17

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -87,10 +87,10 @@ BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --tags_offset 0x00000100
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8937
-#TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_COMPILE := true
 #TARGET_USE_SDCLANG := true
-KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
+#KERNEL_TOOLCHAIN := $(PWD)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
+#TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
